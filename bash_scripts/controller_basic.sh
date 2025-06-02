04e75b743c08f0cbc8dcd8d99cf8879a4969387d
mhilmir <rusydiansyahhilmi@gmail.com>
2025-06-02 20:09:52 +0700

    modify bash scripts to universal use

--- bash_scripts/controller_basic.sh
+++ bash_scripts/controller_basic.sh
@@ -1,7 +1,7 @@
 # sourcing
 source /opt/ros/noetic/setup.bash
 source /usr/share/gazebo/setup.sh
-source ~/workspace/devel/setup.bash
+source devel/setup.bash
 
 # launch file
 roslaunch open_manipulator_controller open_manipulator_controller.launch usb_port:=/dev/ttyACM0 baud_rate:=1000000
--- bash_scripts/keyboard.sh
+++ bash_scripts/keyboard.sh
@@ -1,7 +1,7 @@
 # sourcing
 source /opt/ros/noetic/setup.bash
 source /usr/share/gazebo/setup.sh
-source ~/workspace/devel/setup.bash
+source devel/setup.bash
 
 # launch file
 roslaunch open_manipulator_teleop open_manipulator_teleop_keyboard.launch
