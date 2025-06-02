9594096922dc1af6f5778cd6792be4048ad94038
mhilmir <rusydiansyahhilmi@gmail.com>
2025-06-02 20:15:17 +0700

    modify bash scripts to open ttyACM0 access port

--- bash_scripts/controller_basic.sh
+++ bash_scripts/controller_basic.sh
@@ -3,5 +3,8 @@ source /opt/ros/noetic/setup.bash
 source /usr/share/gazebo/setup.sh
 source devel/setup.bash
 
+# open port access
+sudo chmod 777 /dev/ttyACM0
+
 # launch file
 roslaunch open_manipulator_controller open_manipulator_controller.launch usb_port:=/dev/ttyACM0 baud_rate:=1000000
--- bash_scripts/controller_custom.sh
+++ bash_scripts/controller_custom.sh
@@ -3,5 +3,8 @@ source /opt/ros/noetic/setup.bash
 source /usr/share/gazebo/setup.sh
 source devel/setup.bash
 
+# open port access
+sudo chmod 777 /dev/ttyACM0
+
 # launch file
 roslaunch my_command_arm my_launch.launch usb_port:=/dev/ttyACM0 baud_rate:=1000000
