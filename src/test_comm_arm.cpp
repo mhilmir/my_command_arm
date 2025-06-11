d334fbf2dc2eedc1d8c73bc2ee2c8766f417adac
mhilmir <rusydiansyahhilmi@gmail.com>
2025-06-11 10:23:34 +0700

    logic komunikasi dua node done yaa

--- src/test_comm_arm.cpp
+++ src/test_comm_arm.cpp
@@ -1,19 +1,13 @@
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
 
-// bool active = false;
-// bool sent = false;
-// ros::Time start_time;
 bool toggle_data;
+bool active;
 
 void callback(const std_msgs::Bool::ConstPtr& msg) {
-    // if (msg->data) {
-    //     ROS_INFO("Node B: Starting operation.");
-    //     active = true;
-    //     sent = false;
-    //     start_time = ros::Time::now();
-    // }
+
     toggle_data = msg->data;
+    active = toggle_data;
 }
 
 int main(int argc, char** argv) {
@@ -23,28 +17,14 @@ int main(int argc, char** argv) {
     ros::Publisher pub = nh.advertise<std_msgs::Bool>("/toggle_node", 1);
     ros::Subscriber sub = nh.subscribe("/toggle_node", 1, callback);
 
-    // ROS_INFO("Node B started and waiting for activation.");
-
-    std_msgs::Bool msg; 
+    std_msgs::Bool msg;
+    active = false;
 
     ros::Rate rate(10);
     while (ros::ok()) {
-        // if (active) {
-        //     ROS_INFO_THROTTLE(1, "Node B is running...");
-        //     if (!sent && (ros::Time::now() - start_time).toSec() > 5.0) {
-        //         std_msgs::Bool msg;
-        //         msg.data = false;
-        //         pub.publish(msg);
-        //         ROS_INFO("Node B: Handoff back to Node A.");
-        //         active = false;
-        //         sent = true;
-        //     }
-        // }
-        // ros::spinOnce();
-        // rate.sleep();
 
         ROS_INFO("wait for the quadruped finish the navigation_1 job");
-        while(ros::ok() && !(toggle_data==true)){
+        while(ros::ok() && (active==false)){
             ROS_INFO("...");
             ros::spinOnce();
             rate.sleep();
@@ -52,12 +32,13 @@ int main(int argc, char** argv) {
         ROS_INFO("quadruped has been finished the navigation_1 job\n");
 
         ROS_INFO("arm grasp the object");
-        ros::Duration(3.0).sleep();
+        ros::Duration(4.0).sleep();
+        active = false;
         msg.data = false; pub.publish(msg);
         ROS_INFO("handoff to quadruped\n");
         
         ROS_INFO("wait for the quadruped finish the navigation_2 job");
-        while(ros::ok() && !(toggle_data==true)){
+        while(ros::ok() && (active==false)){
             ROS_INFO("...");
             ros::spinOnce();
             rate.sleep();
@@ -65,7 +46,8 @@ int main(int argc, char** argv) {
         ROS_INFO("quadruped has been finished the navigation_2 job\n");
 
         ROS_INFO("arm place the object");
-        ros::Duration(3.0).sleep();
+        ros::Duration(4.0).sleep();
+        active = false;
         msg.data = false; pub.publish(msg);
         ROS_INFO("handoff to quadruped\n");
         
